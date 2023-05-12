b043498c5f91ba331ff77e69214bbf706c5e967a
Marlon <marlonwason100@gmail.com>
2023-05-12 00:03:08 -0400

    add game end

--- script.js
+++ script.js
@@ -7,30 +7,51 @@ const player = (name, marker) => ({ name, marker });
 const gameBoard = (() => {
   const board = ['', '', '', '', '', '', '', '', ''];
 
+  const checkWinner = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const makeMove = (index, marker) => {
     board[index] = marker;
-
     console.log('gameBoard module loaded');
 
-    const checkWinner = () => {
-    // check for winning combinations and return the winner's symbol
-    // or null if there is no winner
-    };
-
-    return { makeMove, checkWinner };
+    return { makeMove };
   };
-  return { makeMove };
+  // iterate over each winning combination in array
+  const checkWin = (player) => checkWinner.some(
+    (winningCombo) => winningCombo.every(
+      (index) => board[index] === player.marker,
+      // check whether all the indexes in that combination are occupied
+      // by the player's marker.
+    ),
+  );
+
+  return { makeMove, checkWin };
 })();
 
 // Main code
-const cells = document.querySelectorAll('.cell');
-console.log(cells);
-const player1 = player('Player 1', 'X');
-const player2 = player('Player 2', 'O');
-let currentPlayer = player1;
+const game = () => {
+  const cells = document.querySelectorAll('.cell');
+  const message = document.querySelector('.message');
+  const player1 = player('Player 1', 'X');
+  const player2 = player('Player 2', '0');
+  let currentPlayer = player1;
 
-cells.forEach((cell, index) => {
-  cell.addEventListener('click', () => {
+  console.log(cells);
+
+  // event listener for each cell
+  const handleCellClick = (event) => {
+    const cell = event.target;
+    const index = Array.from(cells).indexOf(cell);
+    console.log(Array.from(cells).indexOf(cell));
+
     if (cell.textContent !== '') {
       // do nothing if cell is occupied
       return;
@@ -39,14 +60,27 @@ cells.forEach((cell, index) => {
     gameBoard.makeMove(index, currentPlayer.marker);
 
     cell.textContent = currentPlayer.marker;
+    cell.classList = `cell ${currentPlayer.marker}`;
 
-    const winner = gameBoard.checkWinner;
+    const winner = gameBoard.checkWin(currentPlayer);
 
     if (winner) {
-      // display winner
+      message.textContent = `${currentPlayer.name} Wins!`;
+      cells.forEach((cell) => {
+        cell.removeEventListener('click', handleCellClick);
+      });
     } else {
       currentPlayer = (currentPlayer === player1) ? player2 : player1;
+      message.textContent = `${currentPlayer.name}'s Turn`;
     }
+
     console.log(cells);
+    console.log(currentPlayer);
+  };
+
+  cells.forEach((cell) => {
+    cell.addEventListener('click', handleCellClick);
   });
-});
+};
+
+game();
